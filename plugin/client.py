4e47b67c3ea5e0c49a77915a70c124352431433f
Jack Cherng <jfcherng@gmail.com>
2026-05-23 22:44:42 +0800

    fixup
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/client.py
+++ plugin/client.py
@@ -206,7 +206,6 @@ class CopilotPlugin(LspPlugin):
 
     @override
     def on_initialized_async(self) -> None:
-        """Called once after the `initialized` notification, replaces on_settings_changed."""
         self.update_status_bar_text()
 
         if not (session := self.weaksession()):
